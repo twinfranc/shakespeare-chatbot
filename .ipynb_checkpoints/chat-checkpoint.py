9546220eac47c126d9b765c8160a2c5f96d6ce99
twinfranc <twinixfranc17@gmail.com>
2024-12-09 12:38:47 +0100

    sixth commit

--- .ipynb_checkpoints/chat-checkpoint.py
+++ .ipynb_checkpoints/chat-checkpoint.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Download NLTK resources
-nltk.download('punkt')
+nltk.download('punkt_tab'')
 nltk.download('stopwords')
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
